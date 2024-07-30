f4250acb243549b387b38e3cfde4dd408586a4ef
jmoon0 <moonjoshua3@gmail.com>
2024-07-30 16:22:07 -0700

    Added render database url as default arg
    Added due to error running database locally

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgresql://ufcdle_user:U3yqLBsJXcB0FPVvshzAJcM8bDdnLtzU@dpg-cqhhpo56l47c73fnr7qg-a.ohio-postgres.render.com/ufcdle")
 #postgresql://ufcdle_user:U3yqLBsJXcB0FPVvshzAJcM8bDdnLtzU@dpg-cqhhpo56l47c73fnr7qg-a.ohio-postgres.render.com/ufcdle
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
