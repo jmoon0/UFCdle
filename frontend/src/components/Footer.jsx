3ad592aaf5dcd299d4c8df4f3225e377c38c8eba
jmoon0 <moonjoshua3@gmail.com>
2024-07-31 16:36:18 -0700

    Added contact/kofi links to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,6 +5,10 @@ const Footer = () => {
     <footer className='flex flex-col items-end justify-evenly text-xs text-[--muted-foreground]'>
         <p>*Not affiliated with the UFC.</p>
         <p>&copy; Blessdle 2024.</p>
+        <div className="underline space-x-1">
+          <a href="https://forms.gle/s4rGBUxU9VMh4ZzAA" target='_blank' rel='nofollow' className='hover:text-muted-foreground'>Contact</a> 
+          <a href="https://ko-fi.com/moonwater0" target='_blank' rel='nofollow' className='hover:text-muted-foreground'>Support</a>
+        </div>
     </footer>
   )
 }
