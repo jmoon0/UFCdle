7966edd5b481105f388e55c5860cd4cde77c3d39
Joshua Moon <38844366+jmoon0@users.noreply.github.com>
2025-01-16 23:50:18 -0600

    Update config.py
    vercel db link added to config

--- backend/config.py
+++ backend/config.py
@@ -6,8 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgresql://ufcdle_user:U3yqLBsJXcB0FPVvshzAJcM8bDdnLtzU@dpg-cqhhpo56l47c73fnr7qg-a.ohio-postgres.render.com/ufcdle")
-#postgresql://ufcdle_user:U3yqLBsJXcB0FPVvshzAJcM8bDdnLtzU@dpg-cqhhpo56l47c73fnr7qg-a.ohio-postgres.render.com/ufcdle
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgres://neondb_owner:4FKXNUgHO2Pq@ep-muddy-sun-a5ffe9up-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
